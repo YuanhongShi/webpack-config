95b2c49bece2a82483d6199cbdd1057a01876cd4
Yuanhong Shi <shiyuanhong@gmail.com>
2021-11-19 09:15:43 -0700

    add react refresh for development

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
+const plugins = []
+
+if (process.env.NODE_ENV !== 'production') {
+    plugins.push('react-reflesh/babel')
+}
 module.exports = {
     presets: [
         '@babel/preset-env',
         ['@babel/preset-react', { runtime: 'automatic' }],
     ],
+    plugins: plugins,
 }
